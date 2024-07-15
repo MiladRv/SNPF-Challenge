d211250faef394eef849a9f744ec45df47f95cdf
Milad-Pc <m.rivandi@sgity.ir>
2024-07-15 10:30:45 +0330

    feat: properties of product were implemented

--- src/SNPFD.Domain/Products/Product.cs
+++ src/SNPFD.Domain/Products/Product.cs
@@ -2,4 +2,21 @@ namespace SNPFD.Domain.Products;
 
 public sealed class Product : AggregateRoot<Guid>
 {
+    // efcore needs parameterless constructor
+    public Product()
+    {
+        
+    }
+    public Product(string title, uint inventoryCount, ulong price, double discount)
+    {
+        Title = title;
+        InventoryCount = inventoryCount;
+        Price = price;
+        Discount = discount;
+    }
+
+    public string Title { get; private set; }
+    public uint InventoryCount { get; private set; }
+    public ulong Price { get; private set; }
+    public double Discount { get; private set; }
 }
